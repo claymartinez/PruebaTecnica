d59565e5018b4795cf079809ca9d254cd8127f63
Clay Martinez <clay.ms@hotmail.com>
2023-11-17 15:05:15 -0500

    build: Inicializacion del proyecto, instalacion de dependencias.
    creo proyecto con vite, react, tailwind inicialmente.

---
+++ PruebaTecnica/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <p className="flex ">Hola soy prueba tecnica</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
